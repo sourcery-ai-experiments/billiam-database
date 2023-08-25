a5dcf8c7cc19d9ce98cbf636c9e67afa67d81722
Bilbottom <25640332+Bilbottom@users.noreply.github.com>
2023-08-25 12:45:27 +0000

    Deploying to gh-pages from @ Bilbottom/billiam-database@75222b3ecfafda6387519293196690a2d90f9bf9 🚀

--- compiled/billiam_database/billiam_database/models/presentation/pl__transactions_cube.sql
+++ compiled/billiam_database/billiam_database/models/presentation/pl__transactions_cube.sql
@@ -13,6 +13,7 @@ WITH
     ),
 
 final AS (
+    -- noqa: disable=ST06
     SELECT
         GROUPING_ID(
             transaction_date,
@@ -28,12 +29,13 @@ final AS (
         COUNT(DISTINCT transaction_id) AS total_transaction_count,
         COUNT(*) AS total_item_count,
         COUNT(DISTINCT item) AS distinct_item_count,
-        SUM(cost) AS total_cost,
-        AVG(cost) AS average_cost,
-        MIN(cost) AS min_cost,
-        MAX(cost) AS max_cost,
-        STDDEV_POP(cost) AS standard_dev_cost
+        SUM("cost") AS total_cost,
+        AVG("cost") AS average_cost,
+        MIN("cost") AS min_cost,
+        MAX("cost") AS max_cost,
+        STDDEV_POP("cost") AS standard_dev_cost
     FROM int_transaction_items
+    -- noqa: enable=ST06
     
     GROUP BY CUBE(
         transaction_date,
--- compiled/billiam_database/billiam_database/models/staging/stg__daily_tracker.sql
+++ compiled/billiam_database/billiam_database/models/staging/stg__daily_tracker.sql
@@ -2,19 +2,20 @@
 
 WITH
     src_tracker AS (
-        SELECT 
-          /* no columns returned from star() macro */
+        SELECT *
         FROM 'billiam_database/models/source/daily_tracker.csv'
     ),
 
 final AS (
+    -- noqa: disable=ST06
     SELECT
         date_time::TIMESTAMP AS date_time,
-        TRIM(task) AS task,
+        TRIM("task") AS "task",
         COALESCE(TRIM(detail), '') AS detail,
-        "interval"::INTEGER AS interval,
+        "interval"::INTEGER AS "interval",
         TRIM(company) AS company
     FROM src_tracker
+    -- noqa: enable=ST06
 )
 
 SELECT * FROM final
