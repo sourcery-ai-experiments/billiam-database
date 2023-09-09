fb430f0f6dc575794f0c0d5f13b2fe97d243b845
Bilbottom <25640332+Bilbottom@users.noreply.github.com>
2023-09-09 15:50:03 +0000

    Deploying to gh-pages from @ Bilbottom/billiam-database@bf26411026fce65ad1642d00b23c0773a65d6feb 🚀

---
+++ compiled/billiam_database/billiam_database/models/staging/stg__monzo_transactions.sql
@@ -0,0 +1,32 @@
+
+
+WITH
+    src_monzo_transactions AS (
+        SELECT *
+        FROM 'billiam_database/models/source/monzo_transactions.csv'
+    ),
+
+final AS (
+    -- noqa: disable=ST06
+    SELECT
+        "Transaction ID" AS transaction_id,
+        "Date"::DATE AS transaction_date,
+        "Time"::TIME AS transaction_time,
+        TRIM("Type") AS "type",
+        TRIM("Name") AS counterparty,
+        "Emoji" AS emoji,
+        TRIM("Category") AS category,
+        -"Amount"::DECIMAL(18, 2) AS "cost",
+        "Currency" AS currency,
+        -"Local amount"::DECIMAL(18, 2) AS local_cost,
+        "Local currency" AS local_currency,
+        "Notes and #tags" AS notes,
+        "Address" AS address,
+        TRIM("Receipt") AS receipt,
+        TRIM("Description") AS description,
+        "Category split" AS category_split,
+    FROM src_monzo_transactions
+    -- noqa: enable=ST06
+)
+
+SELECT * FROM final
